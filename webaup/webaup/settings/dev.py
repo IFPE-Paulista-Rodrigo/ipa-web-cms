0bea2a30a212d4845272e1a14692b65c90ea4f10
Fábio Júlio <fjss@pop-os>
2023-04-03 20:34:44 -0300

    Atualização webaup.json

--- webaup/webaup/settings/dev.py
+++ webaup/webaup/settings/dev.py
@@ -2,15 +2,6 @@ from .base import *  # noqa: F403, F401
 
 DEBUG = True
 
-EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "pythonlip@gmail.com"
-EMAIL_HOST_PASSWORD = "kikfkawatrdkcamj"
-EMAIL_USE_TLS = True
-EMAIL_FAIL_SILENT = False
-
 # WAGTAILADMIN_BASE_URL required for notification emails
 WAGTAILADMIN_BASE_URL = "http://localhost:8000"
 
